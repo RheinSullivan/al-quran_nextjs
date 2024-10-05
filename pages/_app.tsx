665b0eb6cd160fffff29bde20088f4bd0ae844dd
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-10-05 23:51:17 +0700

    aupdate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,12 @@ import Navbar from "@/pages/layout/Navbar/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [language, setLanguage] = useState("en");
+  const test = "test";
 
   return (
     <div>
       <Navbar language={language} />
+      {test}
       <Component {...pageProps} language={language} />
     </div>
   );
