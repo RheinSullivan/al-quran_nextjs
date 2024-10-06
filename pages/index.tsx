21a323400e535a7b84e635ef4565cbce4c2d393a
rheinsullivan <rheinsullivan06@gmail.com>
2024-10-06 11:34:50 +0700

    gak tau puyeng and blenger

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,8 @@
 import { EB_Garamond } from "next/font/google";
 import { English } from "@/language/English";
 import { Indonesian } from "@/language/Indonesian";
-<<<<<<< HEAD
 import AlQuran from "../src/components/Al-Qur'an/Al-Quran";
 import Bookmarks from "../src/components/LastRead/Bookmarks";
-=======
-import ListSurat from "@/components/home/ListSurat";
-import Header from "@/components/detail/Header";
-import { useState } from "react";
->>>>>>> fe061de58a5c72263b240e64b9eeb4977be3c06c
 
 const ebGaramond = EB_Garamond({
   subsets: ["latin"],
@@ -17,11 +11,8 @@ const ebGaramond = EB_Garamond({
   variable: "--font-eb-garamond",
 });
 
-export default function IndexPage() {
-  const [language, setLanguage] = useState("en");
-
+export default function IndexPage({ language }: { language: string }) {
   return (
-<<<<<<< HEAD
     <section id="home">
       <main className={`flex flex-col bg-kabah bg-cover bg-center backdrop-blur h-screen w-screen items-center justify-center ${ebGaramond.variable}`}>
         <div className="text-center pl-14 md:pl-0 py-72 md:py-64 text-white">
@@ -45,48 +36,5 @@ export default function IndexPage() {
         <Bookmarks language={language} nomorSurah={2} />
       </main>
     </section>
-=======
-    <main id="home">
-      <Header onLanguageChange={setLanguage} />
-      {/* Hero */}
-      <div
-        className={`flex flex-col bg-kabah bg-cover bg-center backdrop-blur h-screen w-screen items-center justify-center ${ebGaramond.variable}`}
-      >
-        <div className="text-center pl-14 md:pl-0 py-72 md:py-64 text-white">
-          <div className="px-4 py-3 text-outline rounded-lg">
-            <h1 className="text-2xl md:text-5xl">
-              ٱلسَّلَامُ عَلَيْكُمْ وَرَحْمَةُ ٱللَّٰهِ وَبَرَكَاتُهُ
-            </h1>
-            <p className="md:text-xl font-semibold italic">Akhi/Ukhti</p>
-            <h3 className="md:text-xl pt-5">
-              {language === "en"
-                ? English.index.subTitle
-                : Indonesian.index.subTitle}
-            </h3>
-          </div>
-          <div className="flex flex-col text-sm md:text-xl">
-            <div className="flex justify-center items-center space-x-3">
-              <a
-                href="#"
-                className="flex md:hidden bg-gray-200 text-black mt-10 px-4 py-2 rounded-md hover:bg-green-600 hover:text-white"
-              >
-                {language === "en"
-                  ? English.header.buttonText
-                  : Indonesian.header.buttonText}
-              </a>
-              <button className="hover:bg-gray-200 py-2 px-4 hover:text-green-600 mt-10 rounded-md bg-green-600 text-white">
-                {language === "en"
-                  ? English.index.buttonTitle
-                  : Indonesian.index.buttonTitle}
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* List Surat */}
-      <ListSurat />
-    </main>
->>>>>>> fe061de58a5c72263b240e64b9eeb4977be3c06c
   );
 }
--- pages/surat/[nomor].tsx
+++ pages/surat/[nomor].tsx
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
 import Surah from "@/src/DetailSurah/Surah";
-=======
-import { useState } from "react";
-import Header from "@/components/detail/Header";
-import Surah from "@/components/detail/Surah";
->>>>>>> fe061de58a5c72263b240e64b9eeb4977be3c06c
 import { useRouter } from "next/router";
 
 const SurahPage = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [language, setLanguage] = useState("en");
   const router = useRouter();
   const { nomor } = router.query;
 
   if (!nomor) return null;
 
-  return (
-    <>
-      <main className="pb-10">
-        <Header onLanguageChange={setLanguage} />
-        <Surah nomorSurah={parseInt(nomor as string, 10)} />
-      </main>
-    </>
-  );
+  return <Surah nomorSurah={parseInt(nomor as string, 10)} />;
 };
 
 export default SurahPage;
