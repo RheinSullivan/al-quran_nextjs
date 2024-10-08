502b0d70c4fb10e84e2a29878ed23dd8e4bbf790
rheinsullivan <rheinsullivan06@gmail.com>
2024-10-09 01:04:32 +0700

    add a pin feature to the Al-Quran.tsx component to be implemented in Bookmarks.tsx

--- pages/layout/Footer.tsx/Footer.tsx
+++ pages/layout/Footer.tsx/Footer.tsx
@@ -5,6 +5,7 @@ export default function Footer() {
     <footer className="flex flex-col text-white justify-center items-center text-xs md:text-base text-center pl-16 md:pl-16 pt-10 pb-12">
       <p>Website ini masih dalam perkembangan, Terimakasih</p>
       <h1>Copyright Rhein Sullivan & 7TogkID</h1>
+      <p>v1.0.1-Beta</p>
     </footer>
   );
 }
